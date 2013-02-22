46e7b53950f1c2ad3059c4610a290710a42433f2
Nitesh Shakya <sorryconnect91@gmail.com>
2013-02-21 19:55:42 -0600

    changing the mainpage

--- src/com/UI/MainPage.java
+++ src/com/UI/MainPage.java
@@ -1,13 +1,9 @@
 package com.UI;
 
 import javax.swing.JFrame;
-import javax.swing.JTable;
 
-public class MainPage extends JFrame {
+public class MainPage extends JFrame{
 	public MainPage(){
-		add(new JTable());
-		setVisible(true);
+		
 	}
-	
-
 }
--- src/com/UI/Runner.java
+++
@@ -1,9 +0,0 @@
-package com.UI;
-
-public class Runner {
-	
-	public static void main(String[] args){
-		new MainPage();
-	}
-
-}
